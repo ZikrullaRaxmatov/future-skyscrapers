40b1f63f5b1975890208cf74b06d4699006f8510
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-03-26 23:25:53 +0900

    #

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -2,50 +2,98 @@ import React from 'react';
 
 function Header(props) {
     return (
-        <div className='Header'>
-            <div className='container'>
-                <nav class="navbar bg-body-tertiary fixed-top">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#!">Offcanvas navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                            <div class="offcanvas-header">
-                                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                            </div>
-                            <div class="offcanvas-body">
-                                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="#!">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#!">Link</a>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Dropdown
+        <div class='header'>
+            <nav class="navbar fixed-top">
+                <div class="container-fluid border border-danger">
+                    <div class="raw d-flex w-100">
+                        <div class="col-6 col-md-12 border border-success">
+                            <ul class="nav nav-underline justify-content-evenly align-items-center d-none d-md-flex ">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#main">Main</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#company">About company</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#services">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-center border border-success" href="#main">
+                                        <h5>Future
+                                            <br />
+                                            Skyscrapers
+                                        </h5>
+                                    </a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href='#projects'>Projects</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href='#news'>News</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href='#communication'>Communication</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href='#communication'>En</a>
+                                </li>
+                            </ul>
+                            <button class="navbar-toggler d-md-none mt-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                        </div>
+                        <div class="col-6 d-flex justify-content-end d-md-none border border-danger ">
+                            <a class="nav-link  d-md-none" href="#!">
+                                <a class="nav-link text-center border border-success" href="#main">
+                                    <h5>Future
+                                        <br />
+                                        Skyscrapers
+                                    </h5>
+                                </a>
+                            </a>
+                            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                                <div class="offcanvas-header">
+                                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+                                        <a class="nav-link text-center border border-success" href="#main">
+                                            <h5>Future
+                                                <br />
+                                                Skyscrapers
+                                            </h5>
                                         </a>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item" href="#!">Action</a></li>
-                                            <li><a class="dropdown-item" href="#!">Another action</a></li>
-                                            <li>
-                                                <hr class="dropdown-divider"/>
-                                            </li>
-                                            <li><a class="dropdown-item" href="#!">Something else here</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                <form class="d-flex mt-3" role="search">
-                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                        <button class="btn btn-outline-success" type="submit">Search</button>
-                                </form>
+                                    </h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div class="offcanvas-body">
+                                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#main">Main</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#company">About company</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#services">Services</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href='#projects'>Projects</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href='#news'>News</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href='#communication'>Communication</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href='#communication'>En</a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </div>
     );
 }
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Main(props) {
     return (
-        <div>
-            Main
+        <div class="main">
+            <div class="container-fluid1 w-1000 border border-danger">
+                <img src='https://res.cloudinary.com/dz9xllyip/image/fetch/c_scale,w_580,dpr_2.0,q_auto:good/https://s3.ap-southeast-2.amazonaws.com/www.urban.com.au/s3fs-public/field/image/2209.c0551afb.jpg' alt='mainImg'/>
+            </div>
         </div>
     );
 }
