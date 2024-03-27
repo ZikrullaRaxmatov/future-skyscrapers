0173ff9e1b6b1e194236ee606bcefd3be1b23cda
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-03-27 22:06:15 +0900

    #

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -4,8 +4,8 @@ import mainImg from '../assets/imageMain.avif'
 function Main(props) {
     return (
         <div class="main border border-danger">
-            <div class="container1">
-                <img class="w-100 " style={{ height: "650px" }} src={mainImg} alt='mainImg' />
+            <img class="w-1000 " style={{ height: "650px" }} src={mainImg} alt='mainImg' />
+            <div class="container">
                 <div class="main-info card-img-overlay">
                     <div class="position-absolute top-50 start-50 ms-5 border border-danger">
                         <p class="text-white fw-bold fst-italic">Excellence in everything</p>
---
+++ src/screens/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Services(props) {
+    return (
+        <div class="services">
+            
+        </div>
+    );
+}
+
+export default Services;
