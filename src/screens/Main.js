acd33b0be57d495f544775a58385f5ba0987cdc2
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-03-31 09:42:41 +0900

    #

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -8,24 +8,11 @@ function Main(props) {
             <div class="container">
                 <div class="main-info">
                     <p>Excellence in everything</p>
-                    {/* <p class="d-none main-info-p-md d-md-block d-lg-none">Excellence in everything</p>
-                    <p class="d-none main-info-p-lg d-lg-block">Excellence in everything</p> */}
+                    <h1>Future <br /> Skyscrapers</h1>
 
-                    <h1 class="d-md-none main-info-h-sm" >Future <br /> Skyscrapers</h1>
-                    <h1 class="d-none main-info-h-md d-md-block d-lg-none " >Future <br /> Skyscrapers</h1>
-                    <h1 class="d-none main-info-h-lg d-lg-block " >Future <br /> Skyscrapers</h1>
-
-                    <div class="d-flex justify-content-center d-md-none ">
-                        <button class=" main-btn-sm btn rounded-pill">Connection</button>
-                        <button class=" main-btn-sm btn main-btn-sm-project rounded-pill">Our projects</button>
-                    </div>
-                    <div class="d-none d-md-flex d-lg-none">
-                        <button class=" main-btn-md btn rounded-pill">Connection</button>
-                        <button class=" main-btn-md btn rounded-pill main-btn-md-project">Our projects</button>
-                    </div>
-                    <div class="d-none d-lg-flex">
-                        <button class=" main-btn-lg btn rounded-pill">Connection</button>
-                        <button class=" main-btn-lg btn rounded-pill main-btn-lg-project">Our projects</button>
+                    <div class="main-btns">
+                        <button class=" btn rounded-pill">Connection</button>
+                        <button class=" btn rounded-pill">Our projects</button>
                     </div>
                 </div>
             </div>
--- src/screens/Services.js
+++ src/screens/Services.js
@@ -3,26 +3,26 @@ import React from 'react';
 function Services() {
     return (
         <div class="services">
-            <div class="bg-color-gradient service-info pb-4">
+            <div class=" service-info pb-4">
                 <div class="container">
-                    <h1 class="text-center text-white pt-4">Our services</h1>
+                    <h1 class="text-center pt-5">Our services</h1>
                     <div class="raw d-md-flex px-3 pt-3">
-                        <div class="service-item col-12 col-md-6 col-lg-8 shadow border border-dark rounded-pill d-flex justify-content-center align-items-center m-2">
-                            <i class="fa-solid fa-circle-info"></i>
+                        <div class="service-item col-12 col-md-6 col-lg-8 shadow border border-primary rounded-pill d-flex justify-content-center align-items-center">
+                            <i class="fa-solid fa-circle-info rounded-pill"></i>
                             <p>You can get <br /> free information </p>
                         </div>
-                        <div class="service-item col-12 col-md-6 col-lg-4  shadow border border-dark rounded-pill d-flex justify-content-center align-items-center m-2">
-                            <i class="fa-solid fa-file-contract fa-2xl"></i>
+                        <div class="service-item col-12 col-md-6 col-lg-4  shadow border border-primary rounded-pill d-flex justify-content-center align-items-center">
+                            <i class="fa-solid fa-file-contract rounded-pill"></i>
                             <p>Documents <br /> clearance</p>
                         </div>
                     </div>
                     <div class="raw d-md-flex px-3 pt-md-4">
-                        <div class="service-item col-12 col-md-6 col-lg-4 shadow border border-dark rounded-pill d-flex justify-content-center align-items-center m-2">
-                            <i class="fa-solid fa-gears fa-2xl"></i>
+                        <div class="service-item col-12 col-md-6 col-lg-4 shadow border border-primary rounded-pill d-flex justify-content-center align-items-center">
+                            <i class="fa-solid fa-gears rounded-pill"></i>
                             <p>Repair <br /> service</p>
                         </div>
-                        <div class="service-item col-12 col-md-6 col-lg-8 shadow border border-dark rounded-pill d-flex justify-content-center align-items-center m-2">
-                            <i class="fa-solid fa-truck-fast fa-2xl"></i>
+                        <div class="service-item col-12 col-md-6 col-lg-8 shadow border border-primary rounded-pill d-flex justify-content-center align-items-center">
+                            <i class="fa-solid fa-truck-fast rounded"></i>
                             <p>Team of <br /> professionals</p>
                         </div>
                     </div>
