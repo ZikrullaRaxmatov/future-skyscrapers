548459d9cb50ccc56daf2c1ad407a1b9002ee629
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-03-31 16:28:56 +0900

    #

--- src/screens/Company.js
+++ src/screens/Company.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import growingImg from '../assets/man.avif'
+import CountUp from 'react-countup';
+
 
 function Company() {
     return (
@@ -10,24 +12,28 @@ function Company() {
             <div class="container">
                 <div class="company-items">
                     <div class="raw">
-                        <h1>Our Company</h1>
+                        <h1 class="mt-1 mt-lg-3">Our Company</h1>
                     </div>
                     <div class="raw company-info">
-                        <div class="col-3">
-                            <h1>120 +</h1>
-                            <p>Number of employees</p>
+                        <div class="left">
+                            <div class="col-3">
+                                <h1> <CountUp end={120} /> +</h1>
+                                <p>Number of employees</p>
+                            </div>
+                            <div class="col-3">
+                                <h1>43 +</h1>
+                                <p>Objects</p>
+                            </div>
                         </div>
-                        <div class="col-3">
-                            <h1>43 +</h1>
-                            <p>Objects</p>
-                        </div>
-                        <div class="col-3">
-                            <h1>250 +</h1>
-                            <p>Special equipment</p>
-                        </div>
-                        <div class="col-3">
-                            <h1>2 +</h1>
-                            <p>14 years on the market</p>
+                        <div class="right">
+                            <div class="col-3 last">
+                                <h1>250 +</h1>
+                                <p>Special equipment</p>
+                            </div>
+                            <div class="col-3 last">
+                                <h1>2 +</h1>
+                                <p>14 years on the market</p>
+                            </div>
                         </div>
                     </div>
                 </div>
